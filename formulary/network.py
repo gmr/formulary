44a2d47a2d477622c189ec08980415a7aa0d611a
Gavin M. Roy <gavinr@aweber.com>
2015-06-04 11:54:15 -0400

    Refactor formulary.network
    - rename Network to NetworkStackTemplate
    - Add resources to itself instead of a template child
    - refactor to use attributes when adding resources to itself
    - Remove a bunch of code

--- formulary/network.py
+++ formulary/network.py
@@ -12,7 +12,7 @@ from formulary import cloudformation
 LOGGER = logging.getLogger(__name__)
 
 
-class Network(object):
+class NetworkStackTemplate(cloudformation.Template):
     """Create a Network cloud-formation stack consisting of the VPC,
     DHCP options, gateway configuration, routing tables, subnets, and network
     ACLs.
@@ -27,241 +27,138 @@ class Network(object):
         :param str config_path: Path to the formulary configuration directory
 
         """
+        super(NetworkStackTemplate, self).__init__()
         self._config_path = config_path
         self._environment = environment
         self._environment_path = path.join(self.PATH_PREFIX, environment)
         self._mappings = self._load_mappings()
         self._network = self._load_config(self._environment_path, 'network')
-        self._template = self._build()
+        self._description = self._network['description']
+        self._vpc, self._vpc_name = self._add_vpc()
+        self._add_dhcp()
+        self._gateway = self._add_gateway()
+        self._internet_gateway = self._add_gateway_attachment()
+        self._route_table = self._add_route_table()
+        self._add_public_route()
+        self._acl = self._add_network_acl()
+        self._add_network_acl_entries()
 
-    def as_json(self):
-        """Return the Network cloud formation JSON
 
-        :rtype: str
-
-        """
-        return self._template.as_json()
-
-    def _add_vpc(self, template):
+    def _add_vpc(self):
         """Add the VPC section to the template, returning the id and name
         for use in the other sections of the stack configuration
 
-        :param template: The template to add the VPC to
-        :type template: formulary.cloudformation.Template
         :rtype: str, str
 
         """
         vpc_id = ''.join(x.capitalize() for x in self._environment.split('-'))
         vpc_name = self._environment.replace('-', '_')
 
-        template.add_resource(vpc_id, _VPC(
+        self.add_resource(vpc_id, _VPC(
             vpc_name, self._network['vpc']['dns-support'],
             self._network['vpc']['dns-hostnames'], self._network['cidr']))
         return vpc_id, vpc_name
 
-    def _add_dhcp(self, template, vpc_id):
+    def _add_dhcp(self):
+        """Add all of the DHCP options to the template for the given VPC"""
+        self._add_dhcp_association(self._add_dhcp_options())
+
+    def _add_dhcp_options(self):
         """Add all of the DHCP options to the template for the given VPC
 
-        :param template: The template to add the VPC to
-        :type template: formulary.cloudformation.Template
-        :param str vpc_id: The VPC to add the DHCP configuration to
-
-        """
-        dhcp_id = self._add_dhcp_options(template, vpc_id)
-        self._add_dhcp_association(template, vpc_id, dhcp_id)
-
-    def _add_dhcp_options(self, template, vpc_id):
-        """Add all of the DHCP options to the template for the given VPC
-
-        :param template: The template to add the VPC to
-        :type template: formulary.cloudformation.Template
-        :param str vpc_id: The VPC to add the DHCP options to
         :rtype: str
 
         """
         config = self._network['dhcp-options']
-        dhcp_id = '{0}Dhcp'.format(vpc_id)
-        template.add_resource(dhcp_id, _DHCPOptions(config['domain-name'],
-                                                    config['name-servers'],
-                                                    config['ntp-servers']))
+        dhcp_id = '{0}Dhcp'.format(self._vpc)
+        self.add_resource(dhcp_id, _DHCPOptions(config['domain-name'],
+                                                config['name-servers'],
+                                                config['ntp-servers']))
         return dhcp_id
 
-    @staticmethod
-    def _add_dhcp_association(template, vpc_id, dhcp_id):
+    def _add_dhcp_association(self, dhcp_id):
         """Add all of the DHCP OptionsAssociation to the template for the given
         VPC and DHCP Options ID.
 
-        :param template: The template to add the VPC to
-        :type template: formulary.cloudformation.Template
-        :param str vpc_id: The VPC to associate the DHCP options with
-        :param str dhcp_id: The DHCP options identifier
+        :param str dhcp_id: The DHCP Options ID
 
         """
-        template.add_resource('{0}Assoc'.format(dhcp_id),
-                              _DHCPOptionsAssociation(dhcp_id, vpc_id))
+        self.add_resource('{0}Assoc'.format(dhcp_id),
+                          _DHCPOptionsAssociation(dhcp_id, self._vpc))
 
-    @staticmethod
-    def _add_gateway(template, vpc_id):
+    def _add_gateway(self):
         """Add a gateway to the template for the specified VPC
 
-        :param template: The template to add the VPC to
-        :type template: formulary.cloudformation.Template
-        :param str vpc_id: The VPC to add the gateway to
         :rtype: str
 
         """
-        gateway_id = '{0}Gateway'.format(vpc_id)
-        template.add_resource(gateway_id, _Gateway())
-        return gateway_id
+        gateway = '{0}Gateway'.format(self._vpc)
+        self.add_resource(gateway, _Gateway())
+        return gateway
 
-    @staticmethod
-    def _add_gateway_attachment(template, vpc_id, gateway_id):
+    def _add_gateway_attachment(self):
         """Attach the specified gateway to the VPC
 
-        :param template: The template to add the VPC to
-        :type template: formulary.cloudformation.Template
-        :param str vpc_id: The VPC to associate the gateway with
-        :param str gateway_id: The gateway
         :rtype: str
 
         """
-        internet_gateway_id = '{0}Attachment'.format(gateway_id)
-        template.add_resource(internet_gateway_id,
-                              _GatewayAttachment(vpc_id, gateway_id))
+        internet_gateway_id = '{0}Attachment'.format(self._gateway)
+        self.add_resource(internet_gateway_id,
+                          _GatewayAttachment(self._vpc, self._gateway))
         return internet_gateway_id
 
-    @staticmethod
-    def _add_network_acl(template, index, acl_id, acl):
-        """Add an entry for the specified ACL
+    def _add_network_acl(self):
+        """Add the Network ACL to the VPC
 
-        :param template: The template to add the VPC to
-        :type template: formulary.cloudformation.Template
-        :param int index: The entry offset in the list
-        :param str acl_id: The ALC to add the entry to
-        :param dict acl: The ACL configuration
+        :rtype: str
 
         """
-        entry_id = '{0}{1}'.format(acl_id, index)
-        template.add_resource(entry_id, _NetworkACLEntry(
-            acl_id, acl['cidr'], acl['number'], acl['protocol'], acl['action'],
-            acl['egress'], acl['ports']))
+        acl = '{0}Acl'.format(self._vpc)
+        self.add_resource(acl, _NetworkACL(self._vpc_name, self._vpc))
+        return acl
 
-    def _add_network_acls(self, template, vpc_id, vpc_name):
-        """Add Network ACLs for the specified VPC
-
-        :param template: The template to add the VPC to
-        :type template: formulary.cloudformation.Template
-        :param str vpc_id: The VPC ID to associate the gateway with
-        :param str vpc_name: The VPC name to associate the gateway with
-
-        """
-        acl_id = '{0}Acl'.format(vpc_id)
-        template.add_resource(acl_id, _NetworkACL(vpc_name, vpc_id))
+    def _add_network_acl_entries(self):
+        """Iterate through the ACL entries and add them"""
         for index, acl in enumerate(self._network['network-acls']):
-            self._add_network_acl(template, index, acl_id, acl)
+            self.add_resource('{0}{1}'.format(self._acl, index),
+                              _NetworkACLEntry(self._acl, acl['cidr'],
+                                               acl['number'], acl['protocol'],
+                                               acl['action'], acl['egress'],
+                                               acl['ports']))
 
-    @staticmethod
-    def _add_public_route(template, vpc_id, route_table_id, gateway_id,
-                          internet_gateway_id):
+    def _add_public_route(self):
         """Add the public route specified in the mapping ``pubic/cidr`` for
         the specified VPC, route table, gateway and internet gateway.
 
-        :param template: The template to add the VPC to
-        :type template: formulary.cloudformation.Template
-        :param str vpc_id: The VPC ID to associate the route with
-        :param str route_table_id: The route table
-        :param str gateway_id: The gateway
-        :param str internet_gateway_id: The internet gateway
-
         """
-        template.add_resource('{0}Route'.format(vpc_id),
-                              _Route(route_table_id,
-                                     {'Fn::FindInMap': ['SubnetConfig',
-                                                        'Public',
-                                                        'CIDR']},
-                                     gateway_id, internet_gateway_id))
+        self.add_resource('{0}Route'.format(self._vpc),
+                          _Route(self._route_table,
+                                 {'Fn::FindInMap': ['SubnetConfig',
+                                                    'Public',
+                                                    'CIDR']},
+                                 self._gateway, self._internet_gateway))
 
-    @staticmethod
-    def _add_route_table(template, vpc_id):
+    def _add_route_table(self,):
         """Add the the route table for the specified VPC
 
-        :param template: The template to add the VPC to
-        :type template: formulary.cloudformation.Template
-        :param str vpc_id: The VPC ID to associate the route table with
         :rtype: str
 
         """
-        route_table_id = '{0}RouteTable'.format(vpc_id)
-        template.add_resource(route_table_id, _RouteTable(vpc_id))
+        route_table_id = '{0}RouteTable'.format(self._vpc)
+        self.add_resource(route_table_id, _RouteTable(self._vpc))
         return route_table_id
 
-    def _add_routing(self, template, vpc_id):
-        """Add all of the routing configuration for the specified VPC
-
-        :param template: The template to add the VPC to
-        :type template: formulary.cloudformation.Template
-        :param str vpc_id: The VPC ID to associate the route table with
-        :rtype: str
-
-        """
-        gateway_id = self._add_gateway(template, vpc_id)
-        internet_gateway_id = self._add_gateway_attachment(template, vpc_id,
-                                                           gateway_id)
-        route_table_id = self._add_route_table(template, vpc_id)
-        self._add_public_route(template, vpc_id, route_table_id, gateway_id,
-                               internet_gateway_id)
-        return route_table_id
-
-    def _add_subnet(self, template, subnet, vpc_id, vpc_name, route_table_id):
-        """Add the subnet for the specified VPC and route table
-
-        :param template: The template to add the VPC to
-        :type template: formulary.cloudformation.Template
-        :param str subnet: The subnet name
-        :param str vpc_id: The VPC ID to associate the subnet with
-        :param str vpc_name: The VPC name
-        :param str route_table_id: The route table
-
-        """
-        subnet_cfg = self._network['subnets'][subnet]
-        subnet_id = '{0}{1}Subnet'.format(vpc_id, subnet)
-        template.add_resource(subnet_id,
-                              _Subnet(vpc_name, subnet, vpc_id,
-                                      subnet_cfg['az'], subnet_cfg['cidr']))
-        subnet_route_association = '{0}Assoc'.format(subnet_id)
-        template.add_resource(subnet_route_association,
-                              _SubnetRouteTableAssociation(subnet_id,
-                                                           route_table_id))
-
-    def _add_subnets(self, template, vpc_id, vpc_name, route_table_id):
-        """Add the network subnets for the specified VPC and route table
-
-        :param template: The template to add the VPC to
-        :type template: formulary.cloudformation.Template
-        :param str subnet: The subnet name
-        :param str vpc_id: The VPC ID to associate the subnets with
-        :param str vpc_name: The VPC name
-        :param str route_table_id: The route table
-
-        """
+    def _add_subnets(self):
+        """Add the network subnets for the specified VPC and route table"""
         for subnet in self._network['subnets']:
-            self._add_subnet(template, subnet, vpc_id, vpc_name, route_table_id)
-
-    def _build(self):
-        """Build the Cloud Formation template for the network stack
-
-        :rtype: formulary.cloudformation.Template
-
-        """
-        template = cloudformation.Template()
-        template.update_mappings(self._mappings)
-        template.set_description(self._network['description'])
-        vpc_id, vpc_name = self._add_vpc(template)
-        self._add_dhcp(template, vpc_id)
-        self._add_network_acls(template, vpc_id, vpc_name)
-        route_table_id = self._add_routing(template, vpc_id)
-        self._add_subnets(template, vpc_id, vpc_name, route_table_id)
-        return template
+            config = self._network['subnets'][subnet]
+            subnet_id = '{0}{1}Subnet'.format(self._vpc, subnet)
+            self.add_resource(subnet_id,
+                              _Subnet(self._vpc_name, subnet, self._vpc,
+                                      config['az'], config['cidr']))
+            self.add_resource('{0}Assoc'.format(subnet_id),
+                              _SubnetRouteTableAssociation(subnet_id,
+                                                           self._route_table))
 
     def _load_config(self, cfg_path, name):
         """Load YAML configuration for the specified name from the path.
