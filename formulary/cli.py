1ff75f85b718bd55c4a81c8bbb80ece4b1d2c6d8
Gavin M. Roy <gavinr@aweber.com>
2015-06-05 13:01:49 -0400

    Add new RDS template type
    - cli -v flag shows JSON doc when submitting
    - create/update RDS stacks

--- formulary/cli.py
+++ formulary/cli.py
@@ -10,6 +10,7 @@ import sys
 from formulary import __version__
 from formulary import cloudformation
 from formulary import network
+from formulary import rds
 from formulary import service
 
 DESCRIPTION = 'AWS Cloud Formation Stack Management'
@@ -19,39 +20,51 @@ def _add_commands(parser):
     commands = parser.add_subparsers(title='Commands', dest='command')
     create = commands.add_parser('create',
                                  help='Create a Cloud Formation stack')
-    create.add_argument('type', choices=['network', 'service'],
+    create.add_argument('type', choices=['network', 'rds', 'service'],
                         help='The type of stack to create')
     create.add_argument('name', help='The name of the stack to create')
 
     update = commands.add_parser('update',
                                  help='Update a Cloud Formation stack')
-    update.add_argument('type', choices=['network', 'service'],
+    update.add_argument('type', choices=['network', 'rds', 'service'],
                         help='The type of stack to update')
     update.add_argument('name', help='The name of the stack to update')
 
 
-def _create_network_stack(region, environment, config_path):
+def _create_network_stack(region, environment, config_path, debug=False):
     _create_stack(region,
-                  network.NetworkStackTemplate(environment, config_path))
+                  network.NetworkTemplate(environment, config_path), debug)
 
 
-def _create_service_stack(region, environment, name, config_path):
-    template = service.ServiceStackTemplate(name, environment, region,
-                                            config_path)
-    _create_stack(region, template)
+def _create_rds_stack(region, environment, name, config_path, debug=False):
+    template = rds.RDSTemplate(name, environment, config_path, region)
+    _create_stack(region, template, debug)
 
-def _update_network_stack(region, environment, config_path):
+
+def _create_service_stack(region, environment, name, config_path, debug=False):
+    template = service.ServiceTemplate(name, environment, config_path, region)
+    _create_stack(region, template, debug)
+
+
+def _update_network_stack(region, environment, config_path, debug=False):
     _update_stack(region,
-                  network.NetworkStackTemplate(environment, config_path))
+                  network.NetworkTemplate(environment, config_path), debug)
 
 
-def _update_service_stack(region, environment, name, config_path):
-    template = service.ServiceStackTemplate(name, environment, region,
-                                            config_path)
-    _update_stack(region, template)
+def _update_rds_stack(region, environment, name, config_path, debug=False):
+    template = rds.RDSTemplate(name, environment, config_path, region)
+    _update_stack(region, template, debug)
 
 
-def _create_stack(region, template):
+def _update_service_stack(region, environment, name, config_path, debug=False):
+    template = service.ServiceTemplate(name, environment, config_path, region)
+    _update_stack(region, template, debug)
+
+
+def _create_stack(region, template, debug):
+    if debug:
+        print('Stack Template: ')
+        print(template.as_json())
     try:
         cloudformation.create_stack(region, template)
     except cloudformation.RequestException as error:
@@ -61,7 +74,10 @@ def _create_stack(region, template):
     sys.stdout.write('Stack Cost Calculator URL: {0}\n'.format(result))
 
 
-def _update_stack(region, template):
+def _update_stack(region, template, debug):
+    if debug:
+        print('Stack Template: ')
+        print(template.as_json())
     try:
         cloudformation.update_stack(region, template)
     except cloudformation.RequestException as error:
@@ -104,19 +120,35 @@ def run():
         if args.type == 'network':
             _create_network_stack(args.region,
                                   args.name,
-                                  args.config_dir)
+                                  args.config_dir,
+                                  args.verbose)
+        elif args.type == 'rds':
+            _create_rds_stack(args.region,
+                              args.environment,
+                              args.name,
+                              args.config_dir,
+                              args.verbose)
         elif args.type == 'service':
             _create_service_stack(args.region,
                                   args.environment,
                                   args.name,
-                                  args.config_dir)
+                                  args.config_dir,
+                                  args.verbose)
     elif args.command == 'update':
         if args.type == 'network':
             _update_network_stack(args.region,
                                   args.name,
-                                  args.config_dir)
+                                  args.config_dir,
+                                  args.verbose)
+        elif args.type == 'rds':
+            _update_rds_stack(args.region,
+                              args.environment,
+                              args.name,
+                              args.config_dir,
+                              args.verbose)
         elif args.type == 'service':
             _update_service_stack(args.region,
                                   args.environment,
                                   args.name,
-                                  args.config_dir)
+                                  args.config_dir,
+                                  args.verbose)
