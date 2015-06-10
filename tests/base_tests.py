399c631e3fa17feb099bdced5f655a5f111c8a76
Gavin M. Roy <gavinr@aweber.com>
2015-06-10 01:30:22 -0400

    Update tests

--- tests/base_tests.py
+++ tests/base_tests.py
@@ -1,5 +1,5 @@
 """
-Cloud Formation (cf) object tests
+Base Class Tests
 
 """
 import json
@@ -8,8 +8,7 @@ try:
 except ImportError:
     import unittest
 
-from formulary import cloudformation
-from formulary import resources
+from formulary import base
 
 VPC_EXPECTATION = {
     'Type': 'AWS::EC2::VPC',
@@ -35,7 +34,7 @@ class ResourceTests(unittest.TestCase):
                 'NtpServers': ['127.0.0.1']
             }
         }
-        resource = resources._Resource('AWS::EC2::DHCPOptions')
+        resource = base.Resource('AWS::EC2::DHCPOptions')
         resource.add_property('DomainName', 'foo')
         resource.add_property('DomainNameServers', ['8.8.4.4', '8.8.8.8'])
         resource.add_property('NtpServers', ['127.0.0.1'])
@@ -51,7 +50,7 @@ class ResourceTests(unittest.TestCase):
                 'GatewayId': {'ref': 'gateway-id'}
             }
         }
-        resource = resources._Resource('AWS::EC2::Route')
+        resource = base.Resource('AWS::EC2::Route')
         resource.add_property('RouteTableId', {'ref': 'routing-table-id'})
         resource.add_property('GatewayId', {'ref': 'gateway-id'})
         resource.add_property('DestinationCidrBlock', '0.0.0.0/0')
@@ -59,7 +58,7 @@ class ResourceTests(unittest.TestCase):
         self.assertDictEqual(resource.as_dict(), expectation)
 
     def test_as_json_with_tags(self):
-        resource = resources._Resource('AWS::EC2::VPC')
+        resource = base.Resource('AWS::EC2::VPC')
         resource.add_property('EnableDnsSupport', True)
         resource.add_property('EnableDnsHostnames', True)
         resource.add_property('CidrBlock', '0.0.0.0/0')
@@ -75,7 +74,7 @@ class ResourceTests(unittest.TestCase):
         })
 
     def test_as_json_with_tags_and_name(self):
-        resource = resources._Resource('AWS::EC2::VPC')
+        resource = base.Resource('AWS::EC2::VPC')
         resource.set_name('vpc-test-name')
         resource.add_property('EnableDnsSupport', True)
         resource.add_property('EnableDnsHostnames', True)
--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -1,4 +1,5 @@
 """
+Utility Tests
 
 """
 try:
