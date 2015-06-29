414bed88afc9f7ae6728eae3179707bb225bd801
Gavin M. Roy <gavinr@aweber.com>
2015-06-29 15:46:50 -0400

    Split out base classes

---
+++ formulary/builders/base.py
@@ -0,0 +1,125 @@
+"""
+Base Builder Class
+
+"""
+import uuid
+
+from formulary import template
+from formulary import utils
+
+
+class Builder(object):
+    """Build CloudFormation stack templates"""
+    def __init__(self, config, name):
+        """Create a new instance of the stack builder
+
+        :param fomulary.builders.Config config: Stack configuration
+        :param str name: The name of the builder/stack resource
+
+        """
+        self._config = config
+        self._name = name
+        self._outputs = {}
+        self._resources = {}
+
+    @property
+    def environment(self):
+        """Return the environment value
+
+        :rtype: str
+
+        """
+        return self._config.environment
+
+    @property
+    def mappings(self):
+        """Return the mappings dictionary for the builder
+
+        :rtype: dict
+
+        """
+        return dict(self._config.mappings)
+
+    @property
+    def name(self):
+        """Return the builder's resource name
+
+        :rtype: str
+
+        """
+        return self._name
+
+    @property
+    def outputs(self):
+        """Return the outputs dictionary for the builder
+
+        :rtype: dict
+
+        """
+        return dict(self._outputs)
+
+    @property
+    def resources(self):
+        """Return the resource dictionary for the builder
+
+        :rtype: dict
+
+        """
+        return dict(self._resources)
+
+    def upload(self):
+        """Upload a template for this builder to S3, returning the ID and
+        the URL to the template
+
+        :rtype: str, str
+
+        """
+        stack = template.Template(utils.camel_case(self.name))
+        stack.update_mappings(self._config.mappings)
+        stack.update_outputs(self.outputs)
+        stack.update_resources(self.resources)
+        value = stack.as_json()
+        template_id = str(uuid.uuid4())
+
+        s3client = s3.S3(self._config.s3_bucket, self._config.s3_prefix)
+        return template_id, s3client.upload(template_id, value)
+
+    def _add_output(self, name, description, value):
+        """Add an output that will be added to the template
+
+        :param str name: The name of the output
+        :param str description: The description of the output
+        :param str|dict value: The value of the output
+
+        """
+        self._outputs[name] = {'Description': description, 'Value': value}
+
+    def _add_resource(self, name, resource):
+        """Add a resource to the template, returning the cloud formation
+        template reference name for the resource.
+
+        :param str name: The underscore delimited resource name
+        :param Resource resource: The resource to add
+        :rtype: str
+
+        """
+        resource_id = utils.camel_case(name)
+        self._resources[resource_id] = resource
+        return resource_id
+
+    def _maybe_replace_with_mapping(self, value):
+        """If the value is a ^map macro, replace the with the value from the
+        mappings dict. For example, if the value is ``^map Foo.Bar.Baz``
+        return self._mappings['Foo']['Bar']['Baz'].
+
+        :param str value: The value to check for the map macro
+        :rtype: str|dict
+        :raises: ValueError
+
+        """
+        if value.startswith('^map '):
+            ref = value[5:].split('.')
+            if len(ref) != 3:
+                raise ValueError('Invalid map reference: {}'.format(value[5:]))
+            return self._config.mappings[ref[0]][ref[1]][ref[2]]
+        return value
---
+++ formulary/resources/base.py
@@ -0,0 +1,103 @@
+"""
+Base Resource and Property Classes
+
+"""
+
+class Property(object):
+    """Cloud Formation Resource Property
+
+    Represents a property of a Cloud Formation resource
+
+    """
+    def __init__(self):
+        self._values = dict()
+
+    def as_dict(self):
+        values = dict(self._values)
+        for key, value in self._values.items():
+            if value is None or value == [] or value == {}:
+                del values[key]
+        return values
+
+
+class Resource(object):
+    """Cloud Formation Resource
+
+    Represents a Cloud Formation resource as a class
+
+    """
+    tags = True
+
+    def __init__(self, resource_type):
+        """Create a new resource specifying the resource type
+
+        :param str resource_type: Resource type such as ``AWS::EC2::Route``
+
+        """
+        self._attributes = {}
+        self._name = None
+        self._properties = {}
+        self._tags = {}
+        self._type = resource_type
+
+    def add_attribute(self, name, value):
+        """Add a top-level attribute to the resource such as ``DependsOn`` in
+        ``AWS::EC2::Route``
+
+        :param str name: The attribute name
+        :param str value: The attribute value
+
+        """
+        self._attributes[name] = value
+
+    def add_property(self, name, value):
+        """Add a property to the resource
+
+        :param str name: The name of the property
+        :param str|dict|list|bool|int value: The property value
+
+        """
+        self._properties[name] = value
+
+    def add_tag(self, name, value):
+        """Add a tag to the resource
+
+        :param str name: The name of the tag
+        :param str value: The tag value
+
+        """
+        self._tags[name] = value
+
+    def as_dict(self):
+        """Return the resource as a dictionary for building the cloud-formation
+        configuration.
+
+        :return: dict
+
+        """
+        self._prune_empty_properties()
+        dict_val = dict({'Type': self._type, 'Properties': self._properties})
+        if self._tags and self.tags is True:
+            dict_val['Properties']['Tags'] = []
+            for key, value in self._tags.items():
+                dict_val['Properties']['Tags'].append({'Key': key,
+                                                       'Value': value})
+            if self._name:
+                dict_val['Properties']['Tags'].append({'Key': 'Name',
+                                                       'Value': self._name})
+        for key, value in self._attributes.items():
+            dict_val[key] = value
+        return dict_val
+
+    def set_name(self, name):
+        """Set the resource name for use as a tag
+
+        :param str name: The resource name
+
+        """
+        self._name = name
+
+    def _prune_empty_properties(self):
+        for key, value in list(self._properties.items()):
+            if self._properties[key] is None:
+                del self._properties[key]
