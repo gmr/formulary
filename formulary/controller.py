a0e4b21bef4ceaf26335a860d3d3a193df5de4dd
Gavin M. Roy <gavinr@aweber.com>
2015-06-30 14:37:30 -0400

    Add stack and use new formulary.config.ResourceConfig

--- formulary/controller.py
+++ formulary/controller.py
@@ -3,32 +3,23 @@ Main Formulary Controller
 
 """
 import logging
-from os import path
 import sys
 import uuid
 
-import yaml
+from os import path
 
-from formulary.builders import config
 from formulary.builders import environment
 from formulary.builders import rds
 from formulary.builders import service
-
+from formulary.builders import stack
 from formulary import cloudformation
-from formulary import stack
+from formulary import config
 from formulary import template
 
 LOGGER = logging.getLogger(__name__)
 
 ACTIONS = {'create', 'update'}
-CONFIG_FILES = {'amis.yaml', 'mapping.yaml', 'instances.yaml',
-                'environments', 'rds', 'services'}
 RESOURCE_TYPES = {'environment', 'service', 'elasticache', 'rds',  'stack'}
-STACK_FOLDERS = {'elasticache': 'elasticache',
-                 'environment': 'environments',
-                 'rds': 'rds',
-                 'service': 'services',
-                 'stack': 'stacks'}
 
 
 class Controller(object):
@@ -36,12 +27,17 @@ class Controller(object):
 
     def __init__(self, config_path, action, env, resource_type, resource,
                  verbose, dry_run, profile):
-        config_path = self._normalize_path(config_path)
+        self._config_obj = config.ResourceConfig(config_path,
+                                                 resource_type,
+                                                 resource,
+                                                 env)
+
         self._validate_arguments(config_path, action, env,
                                  resource_type, resource)
 
         self._action = action
         self._s3_prefix = str(uuid.uuid4())
+        LOGGER.debug('S3 prefix: %s', self._s3_prefix)
         self._config_path = config_path
         self._environment = env
         self._profile = profile
@@ -50,10 +46,10 @@ class Controller(object):
         self._verbose = verbose
         self._dry_run = dry_run
 
-        self._amis = self._load_config_file('.', 'amis')
-        self._instances = self._load_config_file('.', 'instances')
-        self._config = self._load_config()
-        self._environment_config = self._load_environment_config()
+        self._amis = self._config_obj.load_file('.', 'amis')
+        self._instances = self._config_obj.load_file('.', 'instances')
+        self._config = self._config_obj.load()
+        self._environment_config = self._config_obj.environment_config()
         self._s3_bucket = self._environment_config.get('s3bucket')
         if not self._s3_bucket:
             self._s3_bucket = self._config.get('s3bucket')
@@ -63,9 +59,9 @@ class Controller(object):
         if not self._s3_bucket:
             raise ValueError('s3bucket not configured in environment')
 
-        self._mappings = self._load_mappings()
+        self._mappings = self._config_obj.mappings()
         self._template = template.Template(self._template_name)
-        self._stack = self._get_stack()
+        self._environment_stack = self._get_environment_stack()
         self._cloud_formation = cloudformation.CloudFormation(self._profile,
                                                               self._region,
                                                               self._s3_bucket,
@@ -73,7 +69,7 @@ class Controller(object):
 
     def execute(self):
         """Create or update a Cloud Formation stack"""
-        builder = self._build_template_resources()
+        self._build_template_resources()
 
         if self._config.get('description'):
             self._template.set_description(self._config['description'])
@@ -105,29 +101,33 @@ class Controller(object):
                 self._error(str(error))
 
     def _build_environment_resources(self):
-        builder_config = config.Config(self._config, self._mappings,
-                                       self._region, self._s3_bucket,
-                                       self._s3_prefix, self._profile)
+        builder_config = config.BuilderConfig(self._config, self._mappings,
+                                              self._region, self._s3_bucket,
+                                              self._s3_prefix, self._profile)
         builder = environment.Environment(builder_config, self._resource)
         self._template.update_outputs(builder.outputs)
         self._template.update_resources(builder.resources)
 
     def _build_rds_resources(self):
-        builder_config = config.Config(self._config, self._mappings,
-                                       self._region, self._s3_bucket,
-                                       self._s3_prefix, self._profile)
-        builder = rds.RDS(builder_config, self._resource, self._stack)
+        builder = rds.RDS(self._get_builder_config(), self._resource,
+                          self._environment_stack)
         self._template.update_outputs(builder.outputs)
         self._template.update_resources(builder.resources)
 
     def _build_service_resources(self):
-        builder_config = config.Config(self._config, self._mappings,
-                                       self._region, self._s3_bucket,
-                                       self._s3_prefix, self._profile,
-                                       self._environment, self._resource)
-        service_path = path.join(self._config_path, self._resource_folder)
-        builder = service.Service(builder_config, self._resource, self._amis,
-                                  service_path, self._stack)
+        service_path = path.join(self._config_path,
+                                 self._config_obj.resource_folder)
+        builder = service.Service(self._get_builder_config(),
+                                  self._resource, self._amis,
+                                  service_path, self._environment_stack)
+        self._template.update_outputs(builder.outputs)
+        self._template.update_resources(builder.resources)
+        return builder
+
+    def _build_stack_resources(self):
+        builder = stack.Stack(self._get_builder_config(),
+                              self._resource, self._config_obj.base_path,
+                              self._amis, self._environment_stack)
         self._template.update_outputs(builder.outputs)
         self._template.update_resources(builder.resources)
         return builder
@@ -137,12 +137,20 @@ class Controller(object):
         if self._resource_type == 'environment':
             return self._build_environment_resources()
 
-        self._template.update_mappings(self._stack.mappings)
+        self._template.update_mappings(self._environment_stack.mappings)
 
         if self._resource_type == 'rds':
             return self._build_rds_resources()
         elif self._resource_type == 'service':
             return self._build_service_resources()
+        elif self._resource_type == 'stack':
+            return self._build_stack_resources()
+
+    def _get_builder_config(self):
+        return config.BuilderConfig(self._config, self._mappings,
+                                    self._region, self._s3_bucket,
+                                    self._s3_prefix, self._profile,
+                                    self._environment, self._resource)
 
     @staticmethod
     def _error(message):
@@ -154,117 +162,15 @@ class Controller(object):
         sys.stderr.write('ERROR: {0}\n'.format(message))
         sys.exit(1)
 
-    def _flatten_config(self, cfg):
-        """Take a given config dictionary and if it contains environment
-        specific values, map the environment values to the associated
-        top level keys.
-
-        :param dict cfg: The configuration to flatten
-        :rtype: dict
-
-        """
-        output = {}
-        for key, value in cfg.items():
-            if isinstance(value, dict) and self._environment in value:
-                output[key] = value[self._environment]
-            else:
-                output[key] = value
-        return output
-
-    def _get_stack(self):
+    def _get_environment_stack(self):
         """Return a stack instance if the environment is not
 
         """
         if self._resource_type == 'environment':
             return
-        return stack.Stack(self._environment, self._environment_config,
-                           None, self._profile)
-
-    def _load_config(self):
-        """Return the config for the specified resource type
-
-        :rtype: dict
-
-        """
-        if self._resource_type in ['environment', 'service']:
-            settings = self._load_config_file(self._resource_folder,
-                                              self._resource_type)
-        else:
-            settings = self._load_config_file(self._resource_folder,
-                                              self._resource)
-        return self._flatten_config(settings)
-
-    def _load_config_file(self, folder, file):
-        """Return the contents of the specified configuration file in the
-        specified configuration folder.
-
-
-        :param str folder: The folder to load the configuration file from
-        :param str file: The file to load
-        :rtype: dict
-
-        """
-        file_path = path.join(self._config_path, folder,
-                              '{}.yaml'.format(file))
-        if path.exists(file_path):
-            LOGGER.debug('Loading configuration from %s', file_path)
-            with open(file_path, 'r') as handle:
-                return yaml.load(handle)
-        LOGGER.debug('Configuration file not found: %s', file_path)
-        return {}
-
-    def _load_environment_config(self):
-        """Return the environment configuration
-
-        :rtype: dict
-
-        """
-        if not self._environment:
-            return {}
-        return self._load_config_file(path.join(STACK_FOLDERS['environment'],
-                                                self._environment),
-                                      'environment')
-
-    def _load_environment_mappings(self):
-        """Return the mappings from the environment folder
-
-        :rtype: dict
-
-        """
-        if not self._environment:
-            return {}
-        return self._load_config_file(path.join(STACK_FOLDERS['environment'],
-                                                self._environment),
-                                      'mappings')
-
-    def _load_mappings(self):
-        """Return the mapping data from the various config dirs and return
-        merged mapping values in order of precedence of global, environment,
-        or service/entity.
-
-        :rtype: dict
-
-        """
-        mappings = dict()
-        mappings.update(self._load_config_file(self._config_path, 'mappings'))
-        if not self._resource == 'environment':
-            mappings.update(self._load_environment_mappings())
-
-        mappings.update(self._load_config_file(self._resource_folder,
-                                               'mappings'))
-
-        return mappings
-
-    @staticmethod
-    def _normalize_path(value): # pragma: no cover
-        """Normalize the specified path value returning the absolute
-        path for it.
-
-        :param str value: The path value to normalize
-        :rtype: str
-
-        """
-        return path.abspath(path.normpath(value))
+        return cloudformation.EnvironmentStack(self._environment,
+                                               self._environment_config,
+                                               None, self._profile)
 
     @property
     def _region(self):
@@ -275,18 +181,6 @@ class Controller(object):
 
         """
         return self._environment_config.get('region', 'us-east-1')
-
-    @property
-    def _resource_folder(self):
-        """Return the folder that contains the resource's configuration data
-
-        :rtype: str
-
-        """
-        if self._resource_type in ['environment', 'service']:
-            return path.join('{}s'.format(self._resource_type),
-                             self._resource)
-        return self._resource_type
 
     @property
     def _template_name(self):
@@ -300,8 +194,7 @@ class Controller(object):
                                         self._resource)
         return self._resource
 
-    @classmethod
-    def _validate_arguments(cls, config_path, action, env,
+    def _validate_arguments(self, config_path, action, env,
                             resource_type, resource):
         """Validate the initialization arguments, raising ``ValueError`` if any
         do not validate.
@@ -309,22 +202,24 @@ class Controller(object):
         :raises: ValueError
 
         """
-        if not cls._validate_action(action):
+        if not self._validate_action(action):
             raise ValueError('Invalid action: {0}'.format(action))
 
-        if not cls._validate_resource_type(resource_type):
+        if not self._validate_resource_type(resource_type):
             raise ValueError('Invalid resource type: {0}'.format(resource_type))
 
-        if not cls._validate_config_path(config_path):
+        if not self._config_obj.validate_config_path(config_path):
             raise ValueError('Invalid config path: {0}'.format(config_path))
 
         if resource_type != 'environment':
             if not env:
                 raise ValueError('Environment not specified')
-            elif not cls._validate_environment(config_path, env):
+            elif not self._config_obj.validate_environment(config_path, env):
                 raise ValueError('Invalid environment: {0}'.format(env))
 
-        if not cls._validate_resource(config_path, resource_type, resource):
+        if not self._config_obj.validate_resource(config_path,
+                                                  resource_type,
+                                                  resource):
             raise ValueError('Invalid resource: {0}'.format(resource))
 
     @staticmethod
@@ -336,54 +231,6 @@ class Controller(object):
 
         """
         return action in ACTIONS
-
-    @staticmethod
-    def _validate_config_path(config_path):
-        """Validate that the specified configuration path exists and
-        contains at least some of the files or folders expected.
-
-        :param str config_path: The path to validate
-        :rtype: bool
-
-        """
-        paths_found = [path.exists(config_path)]
-        paths_found += [path.exists(path.join(config_path, f)) for f in
-                        CONFIG_FILES]
-        return any(paths_found)
-
-    @staticmethod
-    def _validate_environment(config_path, env):
-        """Validate that the expected environment ``network.yaml``
-        configuration file exists within the config path for the environment.
-
-        :param str config_path: The base config path
-        :param str env: The environment name
-        :rtype: bool
-
-        """
-        if not env:
-            return False
-        return path.exists(path.join(config_path,
-                                     STACK_FOLDERS['environment'],
-                                     env, 'environment.yaml'))
-
-    @staticmethod
-    def _validate_resource(config_path, resource_type, resource):
-        """Validate the resource configuration directory exists
-
-        :param str resource_type: The resource type specified
-        :param str resource: The resource to validate
-        :rtype: bool
-
-        """
-        if resource_type in ['environment', 'service']:
-            return path.exists(path.join(config_path,
-                                         '{}s'.format(resource_type),
-                                         resource,
-                                         '{}.yaml'.format(resource_type)))
-        return path.exists(path.join(config_path,
-                                     STACK_FOLDERS[resource_type],
-                                     '{}.yaml'.format(resource)))
 
     @staticmethod
     def _validate_resource_type(resource_type):
