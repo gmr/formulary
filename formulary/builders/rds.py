e17f2ef7c548692ac382a8750f80efbaf9783d9f
Gavin M. Roy <gavinr@aweber.com>
2015-06-29 23:18:20 -0400

    WIP commit of RDS changes

--- formulary/builders/rds.py
+++ formulary/builders/rds.py
@@ -2,8 +2,8 @@
 Build the resources for a RDS Instance
 
 """
-from formulary import base
-from formulary import resources
+from formulary.resources import rds
+from formulary.builders import base
 from formulary.builders import securitygroup
 from formulary import utils
 
@@ -18,41 +18,38 @@ DEFAULT_IOPS = 1000
 
 class RDS(base.Builder):
 
-    def __init__(self, config, name, environment, mappings, stack):
-        super(RDS, self).__init__(config, name, environment, mappings)
+    def __init__(self, config, name, stack):
+        super(RDS, self).__init__(config, name)
         self._stack = stack
         self._security_group = self._add_security_group()
         self._add_instance()
 
     def _add_instance(self):
-        subnets = self._add_subnet_groups(self._config)
+        subnets = self._add_subnet_groups()
         if not self._config.get('multi-az'):
             self._config['availability_zone'] = \
                 self._stack.subnets[0].availability_zone
-        name = '{0}-{1}'.format(self._environment, self._name)
-        resource = resources.DBInstance(name, self._config,
+        name = '{0}-{1}'.format(self._config.environment, self._name)
+        resource = rds.DBInstance(name, self._config,
                                         utils.camel_case(subnets),
                                         {'Ref': self._security_group})
+        resource.add_tag('Environment', self._stack.environment)
+        resource.add_tag('Service', self._name)
         self._add_resource(name, resource)
 
     def _add_security_group(self):
         builder = securitygroup.SecurityGroup(self._config,
                                               self._name,
-                                              self._environment,
-                                              self._mappings,
                                               self._stack)
         self._resources.update(builder.resources)
         return utils.camel_case(builder.logical_id)
 
-    def _add_subnet_groups(self, config):
+    def _add_subnet_groups(self):
         subnets = []
-        if config.get('multi-az'):
-            subnets.append(self._stack.subnets[0].id)
-        else:
-            for subnet in self._stack.subnets:
-                subnets.append(subnet.id)
-        subnet_group_name = '{0}-{1}-subnet-group'.format(self._environment,
-                                                          self._name)
-        resource = resources.DBSubnetGroup(subnet_group_name, subnets)
-        self._add_resource(subnet_group_name, resource)
-        return subnet_group_name
+        for subnet in self._stack.subnets:
+            subnets.append(subnet.id)
+        name = '{0}-{1}-subnet-group'.format(self._config.environment,
+                                             self._name)
+        resource = rds.DBSubnetGroup(name, subnets)
+        self._add_resource(name, resource)
+        return name
