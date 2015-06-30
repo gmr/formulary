b2104964ff2b7d50df11f7f9465631f9283738e8
Gavin M. Roy <gavinr@aweber.com>
2015-06-29 23:06:44 -0400

    Add update_stack and dont delete things

--- formulary/cloudformation.py
+++ formulary/cloudformation.py
@@ -3,7 +3,6 @@ Cloud Formation methods and interfaces
 
 """
 import collections
-import json
 import logging
 import uuid
 
@@ -61,8 +60,32 @@ class CloudFormation(object):
             raise RequestException(error)
 
         LOGGER.debug('Created stack ID: %r', result['StackId'])
-        self._s3.delete(template_id)
         return result['StackId']
+
+    def update_stack(self, template, environment, service=None):
+        """Update a stack in the specified region with the given template.
+
+        :param Template template: The template to use
+        :param str environment: The environment to set in a stack tag
+        :param str|None service: The service name to set in a stack tag
+        :raises: RequestException
+
+        """
+        template_id = str(uuid.uuid4())
+        url = self._s3.upload(template_id, template.as_json())
+        tags = [{'Key': 'Environment', 'Value': environment}]
+        if service:
+            tags.append({'Key': 'Service', 'Value': service})
+
+        try:
+            result = self._client.update_stack(StackName=template.name,
+                                               TemplateURL=url,
+                                               Tags=tags)
+        except exceptions.ClientError as error:
+            self._s3.delete(template_id)
+            raise RequestException(error)
+
+        LOGGER.debug('Updated stack ID: %r', result['StackId'])
 
 
 class RequestException(Exception):
