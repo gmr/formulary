8c37f6095cc56cbe68c7545b42ca6e0a804ba04d
Gavin M. Roy <gavinr@aweber.com>
2015-06-29 23:01:36 -0400

    Fix SecurityGroup building

--- formulary/builders/securitygroup.py
+++ formulary/builders/securitygroup.py
@@ -2,36 +2,41 @@
 Build the resources for a Security Group
 
 """
-from formulary import base
-from formulary import resources
+from formulary.builders import base
+from formulary.resources import ec2
 from formulary import utils
 
 class SecurityGroup(base.Builder):
 
-    def __init__(self, config, name, environment, mappings, stack):
-        super(SecurityGroup, self).__init__(config, name, environment, mappings)
+    def __init__(self, config, name, stack):
+        super(SecurityGroup, self).__init__(config, name)
         self._owner = name
-        self._name = '{0}-{1}-security-group'.format(environment, name)
+        self._name = '{0}-{1}-security-group'.format(self._config.environment,
+                                                     name)
         self._stack = stack
-        self.logical_id = self._add_security_group()
+        self._add_security_group()
+        self._add_output('SecurityGroupId',
+                         'The physical ID for the security group',
+                         {'Ref': utils.camel_case(self._name)})
+
+    @property
+    def logical_id(self):
+        return self.name
 
     def _add_security_group(self):
-        if isinstance(self._config.get('security-group'), str):
-            return self._config.get('security-group')
+        if isinstance(self._config.settings.get('security-group'), str):
+            return self._config.settings.get('security-group')
 
         desc = ('Security Group for the {0} '
                 'service in {1}').format(self._owner.capitalize(),
                                          self._stack.environment.capitalize())
-        resource = resources.SecurityGroup(self._name,
-                                           desc,
-                                           self._stack.vpc.id,
-                                           self._build_ingress_rules())
+        resource = ec2.SecurityGroup(self._name, desc, self._stack.vpc.id,
+                                     self._build_ingress_rules())
         self._add_resource(self._name, resource)
-        return self._name
 
     def _build_ingress_rules(self):
         rules = []
-        group = self._config.get('security-group', {})
+        group = self._config.settings.get('security-group', {})
         ingress_rules = list(group.get('ingress', {}))
         for row in ingress_rules:
             try:
@@ -40,7 +45,7 @@ class SecurityGroup(base.Builder):
                 continue
             protocol, from_port, to_port = utils.parse_port_value(port)
             cidr_block = utils.find_in_map(source)
-            rules.append(resources.SecurityGroupRule(protocol,
-                                                     from_port, to_port,
-                                                     cidr_block).as_dict())
+            rules.append(ec2.SecurityGroupRule(protocol,
+                                               from_port, to_port,
+                                               cidr_block).as_dict())
         return rules
